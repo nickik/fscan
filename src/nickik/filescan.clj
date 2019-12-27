301ab77b745a0631210cebbb2cb8a549a96ff5b7
nick <niklaus.zbinden@gmail.com>
2019-12-27 03:08:08 +0100

    First working

--- src/nickik/filescan.clj
+++ src/nickik/filescan.clj
@@ -3,7 +3,6 @@
   (:require [me.raynes.fs :as f]
             [digest :as d]
             [parallel.core :as p]
-            [datahike.api :as dh]
             [clojure.tools.trace :refer :all]))
 
 (def uri (str "datahike:file:///tmp/example"))
@@ -17,73 +16,82 @@
   (assoc (select-keys file [:path-str])
          :path-hash-ref [:hash-str hash-str]))
 
-(defn  to-hash-trx [file]
+(defn to-hash-trx [file]
   (select-keys file [:hash-str]))
 
 (defn resolve-file [file]
-  {:path-str  (. file getAbsolutePath)
+  {:path-str (. file getAbsolutePath)
    :hash-str (d/md5 file)})
 
 (def schema
   [;; File
-   {:db/ident :path-str
-    :db/valueType :db.type/string
-    :db/unique :db.unique/identity
+   {:db/ident       :path-str
+    :db/valueType   :db.type/string
+    :db/unique      :db.unique/identity
     :db/cardinality :db.cardinality/one}
 
-   {:db/ident :path-hash-ref
-    :db/valueType :db.type/ref
+   {:db/ident       :path-hash-ref
+    :db/valueType   :db.type/ref
     :db/cardinality :db.cardinality/one}
+
    ;; Hash
-   {:db/ident :hash-str
-    :db/valueType :db.type/string
+   {:db/ident       :hash-str
+    :db/valueType   :db.type/string
     :db/cardinality :db.cardinality/one
-    :db/unique :db.unique/identity}])
+    :db/unique      :db.unique/identity}])
+
+(defn map-over-v [m f]
+  (into {} (for [[k v] m] [k (f v)])))
+
+(defn enrich [m]
+  {:count (count m)
+   :paths (mapv :path-str m)
+   :hash  (:hash-str (first m))})
+
+(defn jfdalkf [hash-group dfja]
+  #_(println "-> " dfja  " " (type dfja) "   " (int dfja))
+  (hash-map :path (get (:paths hash-group) (int dfja))
+            :operation :delete))
+
+(defn handle-user-request! [hash-group user-input]
+  (->> (clojure.string/split user-input #",")
+       (map #(Integer/parseInt %))
+       (map (partial jfdalkf hash-group))))
+
+(defn print-return [x]
+  (println) x)
 
 (defn -main [& args]
-  (dh/delete-database uri)
-  (dh/create-database uri)
   (let [path (first args)
         structure (file-seq (clojure.java.io/file path))
-        conn (dh/connect uri)
+        files (sequence
+               (comp
+                (remove f/directory?)
+                (remove exclude-paths?)
+                (map resolve-file))
+               structure)
+        by-hash (p/group-by :hash-str files)
         valid-file-seq
-        (sequence
-         (comp
-          (remove f/directory?)
-          (remove exclude-paths?)
-          (map resolve-file))
-         structure)]
-    (println "path: " path)
-    (println "db: " uri)
+        (map-over-v by-hash enrich)
+        commands (atom [])]
+    (doseq [hash-group (vals valid-file-seq)]
+      (when (< 1 (:count hash-group))
+        (doseq [path-tuple (map-indexed (fn [i path]
+                                          [i path]) (:paths hash-group))]
+          (println (first path-tuple) ": " (second path-tuple)))
+        (println)
+        (print "Enter Paths to be removed [Example: 1,2]")
+        (flush)
+        (->> (read-line)
+             print-return
+             (handle-user-request! hash-group)
+             (swap! commands conj))))
+    (doseq [path (map :path (apply concat @commands))]
+      (println "rm " path)
+      (f/delete path))
+    (println)))
 
-    #_(clojure.pprint/pprint valid-file-seq)
-    (dh/transact conn schema)
-    #_(clojure.pprint/pprint (mapv to-hash-trx valid-file-seq))
-    (dh/transact conn (mapv to-hash-trx valid-file-seq))
-    #_(println "--------1---------")
-    #_(clojure.pprint/pprint (mapv to-path-trx valid-file-seq))
-    (dh/transact conn (mapv to-path-trx valid-file-seq))
-    (println "--------2---------")
-
-    (into {}
-          (filter
-            (fn [[_ v]] (< 1 (count v)))                          ;; Only multible uses
-            (group-by (comp :hash-str :path-hash-ref)
-                      (flatten (dh/q '[:find  (pull ?e [:path-str {:path-hash-ref [:hash-str]}])
-                                       :where
-                                       [?e :path-str ?n]]
-                                     @conn)))))
-
-
-
-    ))
-
-#_(dh/q '[:find  (pull ?e [*]) #_(pull ?e [:path-str {:path-hash-ref [:hash-str]}])
-          :where
-          [?e :hash-str ?n]]
-        @(dh/connect uri))
-
+(-main "/home/nick/Desktop")
 #_(-main "/home/nick/Desktop")
-(println "Run ---->")
-(time (-main "/home/nick/alltech/project"))
+#_(time)
 
